2a65bd821a7da9f1144a8123e868cfa3c84c0e71
colewendling <colewendling@utexas.edu>
2020-12-04 00:16:20 -0600

    data-structures hw - Stack, Queue, Map

---
+++ w3d5/DIY_ADTs.rb
@@ -0,0 +1,77 @@
+class Stack
+    attr_reader :internal_array
+
+    def initialize
+        @internal_arr = []
+    end
+
+    def push(el)
+        @internal_arr.push(el)
+        el
+    end
+
+    def pop
+        @internal_arr.pop
+    end
+
+    def peek
+        @internal_arr.last
+    end
+end
+
+class Queue
+
+    attr_reader :underlying_array
+    
+    def initialize
+        @internal_arr = []
+    end
+
+    def endqueue(el)
+        @internal_arr.push(el)
+        el
+    end
+
+    def dequeue
+        @internal_array.shift
+    end
+
+    def peek
+        @internal_arr.first
+    end
+
+end
+
+class Map
+
+    attr_accessor :my_map
+
+    def initialize
+        @my_map = []
+    end
+
+    def set(key, value)
+        @my_map += [[key], [value]]
+    end
+
+    def get(key, value)
+        @my_map.each {|subarray| return subarray[1] if subarray[0] == key }
+    end
+
+    def delete(key)
+        value = get(key)
+        @my_map.reject! {|subarray| subarray[0] == key }
+        value
+    end
+
+    def show
+        deep_dup(@my_map)
+    end
+
+    def deep_dup(arr)
+        arr.map {|el| el.is_a?(Array) ? deep_dep(el) : el}
+    end
+
+end
+
+
