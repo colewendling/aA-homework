38257739c1f6290a44f5f193e49868e64bc9c6ab
colewendling <colewendling@utexas.edu>
2020-12-06 16:23:38 -0600

    comment Line:20 BFS vs DFS Stack utilization // use of recursion.

--- w4d1/graph_nodes_and_bfs.rb
+++ w4d1/graph_nodes_and_bfs.rb
@@ -13,11 +13,11 @@ class GraphNode
   end
 end
 
-def bfs(starting_node, target_value)
+def bfs(starting_node, target_value) 
   queue = [starting_node]
   visited = Set.new()
 
-  until queue.empty?
+  until queue.empty?                  #Creates Queue / Contrary to DFS which utilizes a Stack // Recursion
     node = queue.shift
     unless visited.include?(node)
       return node.val if node.val == target_value
